b8d8111f814327f5dd3e6ca565879f1216fb191f
mohamadusman881 <mohamadusman881@gmail.com>
2022-09-10 16:19:58 +0500

    Apply CRUD operations, searching and pagination.

--- HMS/Areas/Dashboard/Controllers/AccomodationPackagesController.cs
+++ HMS/Areas/Dashboard/Controllers/AccomodationPackagesController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HMS.Areas.ViewModels;
+using HMS.Entities;
+using HMS.Services;
+using HMS.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,129 @@ namespace HMS.Areas.Dashboard.Controllers
 {
     public class AccomodationPackagesController : Controller
     {
-        // GET: Dashboard/AccomodationPackages
-        public ActionResult Index()
+        AccomodationPackagesService accomodationPackagesService = new AccomodationPackagesService();
+        AccomodationTypesService accomodationTypesService = new AccomodationTypesService();
+
+        public ActionResult Index(string searchTerm, int? accomodationTypeID, int? page)
         {
-            return View();
+            int recordSize = 5;
+            page = page ?? 1;
+
+            AccomodationPackagesListingModel model = new AccomodationPackagesListingModel();
+
+            model.SearchTerm = searchTerm;
+            model.AccomodationTypeID = accomodationTypeID;
+
+            model.AccomodationTypes = accomodationTypesService.GetAllAccomodationTypes();
+
+            model.AccomodationPackages = accomodationPackagesService.SearchAccomodationPackages(searchTerm, accomodationTypeID, page.Value, recordSize);
+            var totalRecords = accomodationPackagesService.SearchAccomodationPackagesCount(searchTerm, accomodationTypeID);
+
+            model.Pager = new Pager(totalRecords, page, recordSize);
+
+            return View(model);
         }
+
+
+        #region Create and Edit accomodation type
+        [HttpGet]
+        public ActionResult Action(int? ID)
+        {
+            AccomodationPackageActionModel model = new AccomodationPackageActionModel();
+
+            if (ID.HasValue) //if id has value edit record
+            {
+                var accomodationPackage = accomodationPackagesService.GetAccomodationPackageByID(ID.Value);
+
+                model.ID = accomodationPackage.ID;
+                model.AccomodationTypeID = accomodationPackage.AccomodationTypeID;
+                model.Name = accomodationPackage.Name;
+                model.NoOfRoom = accomodationPackage.NoOfRoom;
+                model.FeePerNight = accomodationPackage.FeePerNight;
+            }
+
+            model.AccomodationTypes = accomodationTypesService.GetAllAccomodationTypes();
+
+            return PartialView("_Action", model);
+        }
+
+        [HttpPost]
+        public JsonResult Action(AccomodationPackageActionModel model)
+        {
+            JsonResult json = new JsonResult();
+            var result = false;
+
+            if (model.ID > 0) //trying to edit a existing record
+            {
+                var accomodationPackage = accomodationPackagesService.GetAccomodationPackageByID(model.ID);
+
+                accomodationPackage.AccomodationTypeID = model.AccomodationTypeID;
+                accomodationPackage.Name = model.Name;
+                accomodationPackage.NoOfRoom = model.NoOfRoom;
+                accomodationPackage.FeePerNight = model.FeePerNight;
+
+                result = accomodationPackagesService.UpdateAccomodationPackage(accomodationPackage);
+            }
+            else //trying to create a record
+            {
+                AccomodationPackage accomodationPackage = new AccomodationPackage();
+
+                accomodationPackage.AccomodationTypeID = model.AccomodationTypeID;
+                accomodationPackage.Name = model.Name;
+                accomodationPackage.NoOfRoom = model.NoOfRoom;
+                accomodationPackage.FeePerNight = model.FeePerNight;
+
+                result = accomodationPackagesService.SaveAccomodationPackage(accomodationPackage);
+            }
+
+
+            if (result)
+            {
+                json.Data = new { Success = true };
+            }
+            else
+            {
+                json.Data = new { Success = false, Message = "Unable to perform action on Accomodation Package." };
+            }
+
+            return json;
+        }
+        #endregion
+
+        #region Delete
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            AccomodationPackageActionModel model = new AccomodationPackageActionModel();
+
+            var accomodationPackage = accomodationPackagesService.GetAccomodationPackageByID(ID);
+
+            model.ID = accomodationPackage.ID;
+
+            return PartialView("_Delete", model);
+        }
+        [HttpPost]
+        public JsonResult Delete(AccomodationPackageActionModel model)
+        {
+            JsonResult json = new JsonResult();
+            var result = false;
+
+            var accomodationPackage = accomodationPackagesService.GetAccomodationPackageByID(model.ID);
+
+            result = accomodationPackagesService.DeleteAccomodationPackage(accomodationPackage);
+
+
+            if (result)
+            {
+                json.Data = new { Success = true };
+            }
+            else
+            {
+                json.Data = new { Success = false, Message = "Unable to Delete Accomodation Package." };
+            }
+
+            return json;
+        }
+        #endregion
     }
 }
